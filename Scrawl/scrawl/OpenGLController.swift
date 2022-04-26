bc2dd60275339f92925d0df85e94bd69a03a0dfc
ItghostFan <ItghostFan@163.com>
2022-04-26 23:14:14 +0800

    画三角形和纹理都支持了，但是还要2个同时画。

--- Scrawl/scrawl/OpenGLController.swift
+++ Scrawl/scrawl/OpenGLController.swift
@@ -13,25 +13,26 @@ import SnapKit
 enum DrawShape {
     case Triangle
     case Rectangle
+    case All
 }
 
 class OpenGLController: UIViewController, GLKViewDelegate {
     
-    private lazy var triangleVertexSize = 3
-    private lazy var rectangleVertexSize = triangleVertexSize * 2
-    private lazy var rectangleVertexColumn = 5
+    private lazy var triangleVertexSize: GLsizei = 3
+    private lazy var rectangleVertexSize: GLsizei = triangleVertexSize * 2
+    private lazy var rectangleVertexColumn: GLsizei = 5
     
     private var glkView = GLKView()
-    private var glkBaseEffect = GLKBaseEffect()
+    private var triangleEffect = GLKBaseEffect()
+    private var rectangleEffect = GLKBaseEffect()
     
     private var drawShape = DrawShape.Rectangle
     
     private var triangleVertexBuffer = UnsafeMutablePointer<GLuint>.allocate(capacity: 1)
-    private lazy var triangleVertices = UnsafeMutablePointer<GLKVector3>.allocate(capacity: triangleVertexSize)
+    private lazy var triangleVertices = UnsafeMutablePointer<GLKVector3>.allocate(capacity: Int(triangleVertexSize))
     
     private var rectangleVertexBuffer = GLuint()
-//    private var rectangleVertices = UnsafeMutablePointer<GLfloat>.allocate(capacity: rectangleVertexSize * rectangleVertexColumn)
-    private var rectangleVertices =
+    private let rectangleVertices: [GLfloat] =      // 注意这里一定要指定类型，不然就不是GLFloat了，无法渲染纹理。
     [
         // bottom right
         0.5, 0.0, 0.0,      1.0, 0.0,
@@ -69,9 +70,6 @@ class OpenGLController: UIViewController, GLKViewDelegate {
         glkView.context = EAGLContext(api: .openGLES2)!
         glkView.delegate = self
         EAGLContext.setCurrent(glkView.context)
-        
-        glkBaseEffect.useConstantColor = GLboolean(GL_TRUE)
-        glkBaseEffect.constantColor = GLKVector4(v: (1.0, 1.0, 1.0, 1.0))
         glClearColor(0.0, 0.0, 0.0, 1.0)
         
         switch drawShape {
@@ -79,6 +77,10 @@ class OpenGLController: UIViewController, GLKViewDelegate {
             makeTriangle()
             break
         case .Rectangle:
+            makeRectangle()
+            break
+        default:
+            makeTriangle()
             makeRectangle()
             break
         }
@@ -95,34 +97,40 @@ class OpenGLController: UIViewController, GLKViewDelegate {
         triangleVertices[2].y = 0.0
         triangleVertices[2].z = 0.0
         
+        triangleEffect.useConstantColor = GLboolean(GL_TRUE)
+        triangleEffect.constantColor = GLKVector4(v: (1.0, 1.0, 1.0, 1.0))
+        
         glGenBuffers(GLsizei(1), triangleVertexBuffer)
         print("Triangle Buffer \(triangleVertexBuffer[0])")
         glBindBuffer(GLenum(GL_ARRAY_BUFFER), triangleVertexBuffer[0])
-        glBufferData(GLenum(GL_ARRAY_BUFFER), MemoryLayout<GLKVector3>.size * triangleVertexSize,         triangleVertices, GLenum(GL_STATIC_DRAW))
+        glBufferData(GLenum(GL_ARRAY_BUFFER), GLsizeiptr(GLsizei(MemoryLayout<GLKVector3>.size) * triangleVertexSize), triangleVertices, GLenum(GL_STATIC_DRAW))
     }
     
     func makeRectangle() {
+        rectangleEffect.useConstantColor = GLboolean(GL_TRUE)
+        rectangleEffect.constantColor = GLKVector4(v: (1.0, 1.0, 1.0, 1.0))
         
         glGenBuffers(GLsizei(1), &rectangleVertexBuffer)
         print("Rectangle Buffer \(rectangleVertexBuffer)")
         glBindBuffer(GLenum(GL_ARRAY_BUFFER), rectangleVertexBuffer)
-        let vertexSize = MemoryLayout<GLfloat>.size * rectangleVertexColumn * rectangleVertexSize
-        glBufferData(GLenum(GL_ARRAY_BUFFER), vertexSize, rectangleVertices, GLenum(GL_STATIC_DRAW))
+        let vertexSize = GLsizei(MemoryLayout<[GLfloat]>.size) * rectangleVertexColumn * rectangleVertexSize
+        glBufferData(GLenum(GL_ARRAY_BUFFER), GLsizeiptr(vertexSize), rectangleVertices, GLenum(GL_STATIC_DRAW))
+        
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.position.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.position.rawValue), GLint(3), GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(MemoryLayout<GLfloat>.size) * rectangleVertexColumn, UnsafeRawPointer(bitPattern: 0))
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.texCoord0.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.texCoord0.rawValue), GLint(2), GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(MemoryLayout<GLfloat>.size) * rectangleVertexColumn, UnsafeRawPointer(bitPattern: MemoryLayout<GLfloat>.size * 3));
+        
         
         let image = UIImage(named: "scrawl")?.cgImage
         do {
             let textureInfo = try GLKTextureLoader.texture(with: image!, options: [GLKTextureLoaderOriginBottomLeft: true, GLKTextureLoaderGrayscaleAsAlpha: true, GLKTextureLoaderApplyPremultiplication: true])
-            glkBaseEffect.texture2d0.name = textureInfo.name
-            glkBaseEffect.texture2d0.enabled = GLboolean(GL_TRUE)
-//            glkBaseEffect.texture2d0.target = GLKTextureTarget(rawValue: textureInfo!.target)!
+            rectangleEffect.texture2d0.name = textureInfo.name
+            rectangleEffect.texture2d0.enabled = GLboolean(GL_TRUE)
+            rectangleEffect.texture2d0.target = GLKTextureTarget(rawValue: textureInfo.target)!
         } catch {
             print("\(error)")
         }
-        
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.position.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.position.rawValue), GLint(3), GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(MemoryLayout<GLfloat>.size * rectangleVertexColumn), UnsafeRawPointer(bitPattern: 0))
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.texCoord0.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.texCoord0.rawValue), GLint(2), GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(MemoryLayout<GLfloat>.size * rectangleVertexColumn), UnsafeRawPointer(bitPattern: MemoryLayout<GLfloat>.size * 3));
     }
     
     deinit {
@@ -146,6 +154,8 @@ class OpenGLController: UIViewController, GLKViewDelegate {
     }
     
     func drawTraingle() {
+        triangleEffect.prepareToDraw()
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.position.rawValue))
         glVertexAttribPointer(
             GLuint(GLKVertexAttrib.position.rawValue),
             GLint(3),
@@ -154,30 +164,29 @@ class OpenGLController: UIViewController, GLKViewDelegate {
             GLsizei(MemoryLayout<GLKVector3>.size),
             UnsafeRawPointer(nil)
         )
-        let vertexSize = GLsizei(triangleVertexSize)
-        glDrawArrays(GLenum(GL_TRIANGLES), GLint(0), vertexSize)
+        glDrawArrays(GLenum(GL_TRIANGLES), GLint(0), triangleVertexSize)
     }
     
     func drawRectangle() {
+        rectangleEffect.prepareToDraw()
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.position.rawValue))
         glVertexAttribPointer(
             GLuint(GLKVertexAttrib.position.rawValue),
             GLint(3),
             GLenum(GL_FLOAT),
             GLboolean(GL_FALSE),
-            GLsizei(MemoryLayout<GLfloat>.size * rectangleVertexColumn),
+            GLsizei(MemoryLayout<GLfloat>.size) * rectangleVertexColumn,
             nil
         )
-        let vertexSize = GLsizei(rectangleVertexSize)
-        glDrawArrays(GLenum(GL_TRIANGLES), GLint(0), vertexSize)
+        glDrawArrays(GLenum(GL_TRIANGLES), GLint(0), rectangleVertexSize)
     }
     
     // MARK: GLKViewDelegate
     
     func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        glClearColor(0.0, 0.0, 0.0, 1.0)
         glClear(GLbitfield(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT))
-        glkBaseEffect.prepareToDraw()
         
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.position.rawValue))
         switch drawShape {
         case .Triangle:
             drawTraingle()
@@ -185,6 +194,11 @@ class OpenGLController: UIViewController, GLKViewDelegate {
         case .Rectangle:
             drawRectangle()
             break
+        default:
+            drawTraingle()
+            drawRectangle()
+            break
         }
     }
+
 }
